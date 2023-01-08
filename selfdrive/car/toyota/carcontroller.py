ba005d34eedffd989ebeaed07066953411412287
alexandresato <sato_alexandre@hotmail.com>
2023-01-08 20:16:30 -0300

    give doors lock capability at carcontroller

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -1,5 +1,6 @@
 from cereal import car
 from common.numpy_fast import clip, interp
+from common.params import Params
 from selfdrive.car import apply_toyota_steer_torque_limits, create_gas_interceptor_command, make_can_msg
 from selfdrive.car.toyota.toyotacan import create_steer_command, create_ui_command, \
                                            create_accel_command, create_acc_cancel_command, \
@@ -17,6 +18,10 @@ MAX_STEER_RATE_FRAMES = 18  # tx control frames needed before torque can be cut
 
 # EPS allows user torque above threshold for 50 frames before permanently faulting
 MAX_USER_TORQUE = 500
+
+params = Params()
+UNLOCK_CMD = b'\x40\x05\x30\x11\x00\x40\x00\x00'
+LOCK_CMD = b'\x40\x05\x30\x11\x00\x80\x00\x00'
 
 
 class CarController:
@@ -92,6 +97,13 @@ class CarController:
     self.last_standstill = CS.out.standstill
 
     can_sends = []
+
+    self.hellobutton = params.get_bool("AleSato_HelloButton")
+    if self.hellobutton and not self.lastHellobutton:
+      can_sends.append(make_can_msg(0x750, UNLOCK_CMD, 0))
+    elif not self.hellobutton and self.lastHellobutton:
+      can_sends.append(make_can_msg(0x750, LOCK_CMD, 0))
+    self.lastHellobutton = self.hellobutton
 
     # *** control msgs ***
     # print("steer {0} {1} {2} {3}".format(apply_steer, min_lim, max_lim, CS.steer_torque_motor)
